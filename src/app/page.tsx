e61c1e33b7d2e91ea45a637910fcf2c359eebef7
Kubabob <j.m.m.bozek@gmail.com>
2025-10-02 12:55:50 +0200

    refactor(page): change variable declarations from let to const for better code clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default async function Home() {
-    let data = await fetch("http://localhost:3000/");
-    let content = await data.text();
+    const data = await fetch("http://localhost:3000/");
+    const content = await data.text();
     return (
         <p>{content}</p>
     );
