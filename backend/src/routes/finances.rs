df8cb638120acf8285919f1b0aefb43559e5773b
Kubabob <j.m.m.bozek@gmail.com>
2025-10-04 17:06:51 +0200

    refactor: remove QuoteResponse struct and update get_quotes_for_ticker return type

--- backend/src/routes/finances.rs
+++ backend/src/routes/finances.rs
@@ -8,35 +8,10 @@ use yahoo_finance_api::Quote;
 use super::super::services::finances::get_quotes;
 use shared::models::QuoteQuery;
 
-#[derive(Serialize)]
-pub struct QuoteResponse {
-    timestamp: i64,
-    open: f64,
-    high: f64,
-    low: f64,
-    close: f64,
-    volume: u64,
-    adjclose: f64,
-}
-
-impl From<Quote> for QuoteResponse {
-    fn from(quote: Quote) -> Self {
-        QuoteResponse {
-            timestamp: quote.timestamp,
-            open: quote.open,
-            high: quote.high,
-            low: quote.low,
-            close: quote.close,
-            volume: quote.volume,
-            adjclose: quote.adjclose,
-        }
-    }
-}
-
 pub async fn get_quotes_for_ticker(
     Path(ticker): Path<String>,
     Query(props): Query<QuoteQuery>,
-) -> (StatusCode, Json<Vec<QuoteResponse>>) {
+) -> (StatusCode, Json<Vec<Quote>>) {
     println!("Quotes for ticker: {:?}", &ticker);
 
     let quotes = get_quotes(&ticker, &props.start, &props.end)
