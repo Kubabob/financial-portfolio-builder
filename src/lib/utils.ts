28404477616628e39e85c6603ed14b49a05ab495
Kubabob <j.m.m.bozek@gmail.com>
2025-10-04 11:48:50 +0200

    style: format code for consistency and improve readability in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export function randomRGBColor() {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+export function backgroundRGBColor(RGBColor: string) {
+    return RGBColor.replace('rgb', 'rgba').replace(')', ', 0.5)')
 }
