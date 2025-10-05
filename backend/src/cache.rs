8a32488bf75a2d8da2db4dd01338cefe31352b6a
Kubabob <j.m.m.bozek@gmail.com>
2025-10-05 20:18:28 +0200

    Add caching layer using moka and lazy_static

---
+++ backend/src/cache.rs
@@ -0,0 +1,19 @@
+use lazy_static::lazy_static;
+use moka::sync::Cache;
+use polars::prelude::*;
+use std::time::Duration;
+use yahoo_finance_api::Quote;
+
+lazy_static! {
+    pub static ref QUOTE_CACHE: Cache<String, Quote> = Cache::builder()
+        .time_to_live(Duration::from_mins(5))
+        .max_capacity(10)
+        .build();
+}
+
+lazy_static! {
+    pub static ref DF_CACHE: Cache<String, DataFrame> = Cache::builder()
+        .time_to_live(Duration::from_mins(5))
+        .max_capacity(10)
+        .build();
+}
