fd685948fbc79541ad40dee2377f99f41f226843
Kubabob <j.m.m.bozek@gmail.com>
2025-10-07 18:49:54 +0200

    update cache size
    max capacity increased from 10 to 100

--- backend/src/cache.rs
+++ backend/src/cache.rs
@@ -7,13 +7,13 @@ use yahoo_finance_api::Quote;
 lazy_static! {
     pub static ref QUOTE_CACHE: Cache<String, Vec<Quote>> = Cache::builder()
         .time_to_live(Duration::from_mins(5))
-        .max_capacity(10)
+        .max_capacity(100)
         .build();
 }
 
 lazy_static! {
     pub static ref DF_CACHE: Cache<String, DataFrame> = Cache::builder()
         .time_to_live(Duration::from_mins(5))
-        .max_capacity(10)
+        .max_capacity(100)
         .build();
 }
