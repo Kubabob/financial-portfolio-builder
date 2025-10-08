c5588342adbb22f408f3fd6ea9b20672e2c00f00
Kubabob <j.m.m.bozek@gmail.com>
2025-10-08 18:50:35 +0200

    cleanup

--- backend/src/services/fetching.rs
+++ backend/src/services/fetching.rs
@@ -46,19 +46,6 @@ pub async fn get_dataframe_service(
     let end = OffsetDateTime::parse(&props.end, &time::format_description::well_known::Rfc3339)
         .expect("failed to parse end datetime");
 
-    // // Check cache first
-    // if props.columns.is_empty() {
-    //     if let Some(cached) = DF_CACHE.get(&format!("{}-{}-{}", ticker, start, end)) {
-    //         return Ok(cached);
-    //     }
-    // } else {
-    //     if let Some(cached) =
-    //         DF_CACHE.get(&format!("{}-{}-{}-{}", ticker, start, end, props.columns))
-    //     {
-    //         return Ok(cached);
-    //     }
-    // }
-
     // Check cache first
     match &props.columns {
         None => {
@@ -81,26 +68,6 @@ pub async fn get_dataframe_service(
     let quotes = resp.quotes()?;
 
     let df;
-
-    // if props.columns.is_empty() {
-    //     df = df_from_quotes(&quotes, None)?;
-
-    //     DF_CACHE.insert(format!("{}-{}-{}", ticker, start, end), df.clone());
-    // } else {
-    //     let columns_vec: Vec<String> = props
-    //         .columns
-    //         .split(',')
-    //         .map(|s| s.trim().to_owned())
-    //         .filter(|s| !s.is_empty())
-    //         .collect();
-
-    //     df = df_from_quotes(&quotes, Some(columns_vec))?;
-
-    //     DF_CACHE.insert(
-    //         format!("{}-{}-{}-{}", ticker, start, end, props.columns),
-    //         df.clone(),
-    //     );
-    // }
 
     match &props.columns {
         None => {
