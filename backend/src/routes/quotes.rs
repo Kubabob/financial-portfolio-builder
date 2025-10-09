ab42297204511a7d52971386b31060c319878930
Kubabob <j.m.m.bozek@gmail.com>
2025-10-09 12:07:24 +0200

    update routes
    remove path for ticker

--- backend/src/routes/quotes.rs
+++ backend/src/routes/quotes.rs
@@ -10,14 +10,11 @@ use crate::services::fetching::get_dataframe_service;
 use shared::models::QuoteQuery;
 
 pub fn router() -> Router {
-    Router::new().route("/dataframes/{ticker}", get(get_dataframe))
+    Router::new().route("/dataframes", get(get_dataframe))
 }
 
-pub async fn get_dataframe(
-    Path(ticker): Path<String>,
-    Query(props): Query<QuoteQuery>,
-) -> (StatusCode, String) {
-    let quotes = get_dataframe_service(&ticker, &props)
+pub async fn get_dataframe(Query(props): Query<QuoteQuery>) -> (StatusCode, String) {
+    let quotes = get_dataframe_service(&props)
         .await
         .expect("Failed to get quotes");
 
